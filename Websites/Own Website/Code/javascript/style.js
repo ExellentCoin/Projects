80c2bd1d0266820e75a8e54b2deeb825974b72af
ExellentCoin <caluwejonasother@gmail.com>
2020-03-08 21:43:58 +0100

    last

--- Websites/Own Website/Code/javascript/style.js
+++ Websites/Own Website/Code/javascript/style.js
@@ -1,8 +1,14 @@
 window.onload = function onLoad() {
     changeShowcaseHeight();
+    AllignButton();
 }
 
 function changeShowcaseHeight(){
     let headerHeight = document.getElementById('header').offsetHeight;
     document.getElementById("showcase").style.height = `${window.innerHeight - headerHeight}px`;
+}
+
+function AllignButton() {
+    let showcaseHeight = document.getElementById('showcase').offsetHeight;
+    document.getElementById('button').style.
 }
