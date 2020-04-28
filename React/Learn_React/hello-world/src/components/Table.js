e9b918e786526f26396a542d8be757ed9c695b68
ExellentCoin <caluwejonasother@gmail.com>
2020-04-28 20:25:11 +0200

    Learned about the lifecycle functions in react and learned about React.Fragment.

---
+++ React/Learn_React/hello-world/src/components/Table.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TBody from './TBody';
+
+const Table = () => {
+    return (
+        <table>
+            <tbody>
+                <TBody />
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
